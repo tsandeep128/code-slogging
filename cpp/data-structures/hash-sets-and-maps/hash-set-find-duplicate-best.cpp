8816b4c757f253597f742b7a10f1591f6def260e
Sandeep Thiruvalluvan <sandeepthiru1996@gmail.com>
2022-02-22 23:42:24 +0530

    Add program to find duplicates using hash set

---
+++ cpp/data-structures/hash-sets-and-maps/hash-set-find-duplicate-best.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        unordered_map<int,int> count;
+        for(int i = 0; i < nums.size() ; i++){
+            if(count[nums[i]] != 0)
+                return true;
+            else
+                count[nums[i]] +=1;
+        }
+        return false;
+    }
+};
