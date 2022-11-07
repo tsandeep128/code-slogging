3ce346fccf79d4d4b7b4e664a55fa36b965ca4d8
tsandeep128 <sandeepthiru1996@gmail.com>
2022-11-07 23:49:54 +0530

    added optimized solution description

--- problems/arrays/MaxAndMinElementInArray.c
+++ problems/arrays/MaxAndMinElementInArray.c
@@ -6,6 +6,19 @@ initial idea: create 2 vars - min & max, store 1st element of array in both, tra
 if element is greater than max - update max, if element is less than min - update min.
 No. of comparisons = (N-1) comparisons for both min & max = 2*(N-1) comparisons
 
+Optimization #1: Compare 1st & 2nd element of array. Store lower value in min, higher value in max - then start traversing
+No. of comparisons = 1+2*(N-2)
+
+Optimization #2: Compare 1st & 2nd element of array. Store lower value in min, higher value in max - then start traversing. 
+While traversing, first only check if element is greater than max - If yes, update max = new element
+- If no, check if element is less than min - If yes, update min = new element
+No. of comparisons = 1+(N-2) best case: ascending order; 1+2*(N-2) worst case: descending order
+
+Method #3: Greedy: Divide into left half and right half. Find min & max of left half and right half. 
+Compare min & max of left half and right half to find overall min & max.
+
+Method #4: Compare in pairs
+
 */
 
 #include<stdio.h>
